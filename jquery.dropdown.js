f3c7d3f5f4e9072c1e5755d09f3f17c155f7ef32
notXX <notxx@qq.com>
2014-07-09 21:14:54 +0800

    initial commit

---
+++ jquery.dropdown.js
@@ -0,0 +1,68 @@
+(function($) {
+if (!$ || !$.fn) return;
+
+var methods = {};
+methods.init = function(options) {
+	var $input = this;
+	if (typeof options === "object")
+		options = $.extend({}, defaults, options);
+	else
+		options = $.extend({}, defaults);
+	$input.wrap("<div class='dropdown'>");
+	var $widget = $input.parent().data("options", options);
+	var $button = $("<span class='dropdown'>").append("<span class='ui-icon ui-icon-triangle-1-s'>").click(function() {
+		var $button = $(this), $icon = $button.find(".ui-icon"), 
+			$widget = $button.closest("div.dropdown"), $input = $widget.find("input.dropdown");
+		var options = $widget.data("options"), transform = $icon.css("transform");
+		if (transform === "none" || transform === "matrix(1, 0, 0, 1, 0, 0)") {
+			$input.dropdown("open");
+			options.open.apply($input);
+			$input.trigger("dropdownopen");
+		} else {
+			$input.dropdown("close");
+			options.close.apply($input);
+			$input.trigger("dropdownclose");
+		}
+		options.always.apply($input);
+		$input.trigger("dropdownalways");
+	});
+	$input.addClass("dropdown")
+	.after($('<div style="clear:both;"></div>'))
+	.after($button);
+	if ($input.data("ui-autocomplete")) {
+		$input.on("dropdownopen", function() { $input.autocomplete("search"); });
+		$input.on("dropdownclose", function() { $input.autocomplete("close"); });
+		$input.on("autocompleteclose", function() { $input.dropdown("close"); });
+	}
+	return this;
+};
+methods.open = function() {
+	var $this = this, $widget = $this.closest("div.dropdown");
+	var $icon = $widget.find(".ui-icon"), transform = $icon.css("transform");
+	$icon.css("transform", "rotate(180deg)");
+};
+methods.close = function() {
+	var $this = this, $widget = $this.closest("div.dropdown");
+	var $icon = $widget.find(".ui-icon"), transform = $icon.css("transform");
+	$icon.css("transform", "none");
+};
+
+var defaults = {
+	open: $.noop,
+	close: $.noop,
+	always: $.noop
+}
+
+$.fn.dropdown = function(method) {
+	if(!this.length) return this;
+	if (typeof(method) == 'string' && method.charAt(0) != '_' && methods[method]) {
+		return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+	} else if (typeof method == 'object' || !method) {
+		methods.init.apply(this, arguments);
+	} else {
+		$.error('Method ' + method + ' does not exist in jQuery.dropdown');
+	}
+	return this;
+};
+
+})(jQuery);
